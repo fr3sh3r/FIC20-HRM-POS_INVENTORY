e2b9b65f0f41ec9384b33a945a9b82c8f36b8ac3
fr3sh3r <edimuljadi1@gmail.com>
2024-08-25 10:24:09 +0700

    Commit Mimiti

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,39 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebarlogo-brand">
+            <img src="{{ asset('img/logo.png') }}" alt="logo" width="70"
+                style="display: block; margin: 0 auto; /* center */
+                {{-- class="shadow-light rounded-circle" --}}>
+        </div>
+
+
+        <div class="sidebar-brand">
+            //<a href="index.html">
+                <a href="home">
+                    <p style="text-align:center">EDIM Management </p>
+                </a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="dashboard">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <ul class="dropdown-menu">
+
+                    <li class=''>
+                        <a class="nav-link" href="{{ route('users.index') }}">Users</a>
+
+                    </li>
+
+                </ul>
+
+
+
+
+
+            </li>
+
+    </aside>
+</div>
